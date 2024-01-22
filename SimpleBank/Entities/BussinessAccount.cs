41cf0bea8165aadca152df7e652b30dbdc426d2b
Vinicius Schnoor <vschnooor@outlook.com>
2024-01-22 16:59:11 -0300

    Initial Commit

---
+++ SimpleBank/Entities/BussinessAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleBank.Entities;
+
+public class BussinessAccount : Account
+{
+    public double LoanLimit { get; set; }
+
+    public BussinessAccount()
+    {
+    }
+
+    public BussinessAccount(int number, string holder, double balance, double loanLimit)
+        : base(number, holder, balance)
+    {
+        LoanLimit = loanLimit;
+    }
+
+    public void Loan(double amount)
+    {
+        if (amount <= LoanLimit)
+        {
+            Balance += amount;
+            LoanLimit -= amount;
+        }
+        else
+        {
+            Console.WriteLine("It is not possible to loan.");
+        }
+    }
+}
---
+++ SimpleBank/Program.cs
@@ -0,0 +1,7 @@
+﻿using SimpleBank.Entities;
+
+BussinessAccount client = new BussinessAccount(0001, "teste", 100, 500);
+
+// client.Balance = 200;
+
+Console.WriteLine(client.Balance);
