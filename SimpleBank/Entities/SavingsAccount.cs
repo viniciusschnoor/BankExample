17cd1ccfdfc9ec0a1d13412fa9c33959445ab37c
Vinicius Schnoor <vschnooor@outlook.com>
2024-01-22 19:04:01 -0300

    Upcasting and Downcasting

---
+++ SimpleBank/Entities/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleBank.Entities;
+
+public class SavingsAccount : Account
+{
+    public double InterestRate { get; set; }
+
+    public SavingsAccount()
+    {
+    }
+
+    public SavingsAccount(int number, string holder, double balance, double interestRate)
+        : base(number, holder, balance)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void UpdateBalace()
+    {
+        Balance += Balance * InterestRate;
+    }
+}
--- SimpleBank/Program.cs
+++ SimpleBank/Program.cs
@@ -1,7 +1,14 @@
 ﻿using SimpleBank.Entities;
 
-BussinessAccount client = new BussinessAccount(0001, "teste", 100, 500);
+Account acc = new Account(1001, "Alex", 0.0);
+BussinessAccount bacc = new BussinessAccount(1002, "Maria", 0.0, 500.0);
 
-// client.Balance = 200;
+// Upcasting
+Account acc1 = bacc;
+Account acc2 = new BussinessAccount(1003, "Bob", 0.0, 200.0);
+Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
 
-Console.WriteLine(client.Balance);
+// Downcasting
+BussinessAccount acc4 = (BussinessAccount)acc2;
+//BussinessAccount acc4 = acc2 as BussinessAccount;
+acc4.Loan(100);
